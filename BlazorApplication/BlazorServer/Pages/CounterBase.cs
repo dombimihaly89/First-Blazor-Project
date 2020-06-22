ecb866c09eef843b7cde10cfed0422127f4c1c7e
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-23 00:58:35 +0200

    Changed the root directory to the parent directory of the original .git file

---
+++ BlazorApplication/BlazorServer/Pages/CounterBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Pages
+{
+    public class CounterBase : ComponentBase
+    {
+        protected int currentCount = 0;
+        protected string fontFamily = "Verdana";
+
+        protected void IncrementCount()
+        {
+            currentCount++;
+        }
+    }
+}
