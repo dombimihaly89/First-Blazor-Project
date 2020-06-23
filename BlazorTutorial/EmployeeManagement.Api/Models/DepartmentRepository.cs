cab50417b24fcca71cb4ff38d0f958aca6083bba
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-23 17:57:01 +0200

    Created IDepartmentRespository interface and also implemented in DepartmentRespository class

---
+++ BlazorTutorial/EmployeeManagement.Api/Models/DepartmentRepository.cs
@@ -0,0 +1,28 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Models
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly AppDbContext appDbContext;
+
+        public DepartmentRepository(AppDbContext appDbContext) 
+        {
+            this.appDbContext = appDbContext;
+        }
+        public async Task<Department> getDepartment(int departmentId)
+        {
+            return await appDbContext.Departments.FirstOrDefaultAsync(dept => dept.DepartmentId == departmentId);
+        }
+
+        public async Task<IEnumerable<Department>> getDepartments()
+        {
+            return await appDbContext.Departments.ToListAsync();
+        }
+    }
+}
---
+++ BlazorTutorial/EmployeeManagement.Api/Models/IDepartmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Models
+{
+    interface IDepartmentRepository
+    {
+    }
+}
