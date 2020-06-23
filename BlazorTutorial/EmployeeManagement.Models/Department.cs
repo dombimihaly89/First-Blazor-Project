ac30e443d5ae3ada80eedd6781532eb2fd135eff
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-23 11:58:28 +0200

    - Added images to static files - Added a new css class to site.css to have the same size of images on   the page - Added EmployeeListBase Class to Pages where I loaded the hardcoded   list in OnInitializedAsync method. - Also added functionality to EmployeeList component to display   Employees in Cards. - Modified the MainLayout component to have a container in the inner   div

--- BlazorTutorial/EmployeeManagement.Models/Department.cs
+++ BlazorTutorial/EmployeeManagement.Models/Department.cs
@@ -5,6 +5,6 @@ namespace EmployeeManagement.Models
     public class Department
     {
         public int DepartmentId { get; set; }
-        public string DepatmentName { get; set; }
+        public string DepartmentName { get; set; }
     }
 }
