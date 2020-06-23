02ac75687ba9c2956076be1594115e3bf815ede8
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-23 16:59:07 +0200

    Added IEmployeeRepository to the Models in the EmployeeManagement.Api

---
+++ BlazorTutorial/EmployeeManagement.Api/Models/IEmployeeRepsitory.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Api.Models
+{
+    interface IEmployeeRepsitory
+    {
+        Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee> GetEmployee(int employeeId);
+        Task<Employee> PostEmployee(Employee employee);
+        Task<Employee> UpdateEmployee(Employee employee);
+        void DeleteEmployee(int employeeId);
+    }
+}
