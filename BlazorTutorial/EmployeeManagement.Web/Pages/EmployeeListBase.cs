4f39e1048af16e96cd08f4dbf34f827bb4047fed
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-25 02:45:57 +0200

    Added Microsoft.AspNetCore.Blazor.HttpClient Nuget package to the project. Injected the EmployeeService into the EmployeeListBase class and also called GetEmployees method that is in this service to retrieve Employees from database.

--- BlazorTutorial/EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ BlazorTutorial/EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagement.Models;
+using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
@@ -9,65 +10,14 @@ namespace EmployeeManagement.Web.Pages
 {
     public class EmployeeListBase : ComponentBase
     {
+        [Inject]
+        public IEmployeeService employeeService { get; set; }
+
         public IEnumerable<Employee> Employees { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
-            await Task.Run(LoadEmployees);
-        }
-
-        private void LoadEmployees()
-        {
-            System.Threading.Thread.Sleep(3000);
-            Employee e1 = new Employee
-            {
-                EmployeeId = 1,
-                FirstName = "John",
-                LastName = "Hastings",
-                Email = "David@pragimtech.com",
-                DateOfBirth = new DateTime(1980, 10, 5),
-                Gender = Gender.Male,
-                DepartmentId = 1,
-                PhotoPath = "images/john.png"
-            };
-
-            Employee e2 = new Employee
-            {
-                EmployeeId = 2,
-                FirstName = "Sam",
-                LastName = "Galloway",
-                Email = "Sam@pragimtech.com",
-                DateOfBirth = new DateTime(1981, 12, 22),
-                Gender = Gender.Male,
-                DepartmentId = 2,
-                PhotoPath = "images/sam.jpg"
-            };
-
-            Employee e3 = new Employee
-            {
-                EmployeeId = 3,
-                FirstName = "Mary",
-                LastName = "Smith",
-                Email = "mary@pragimtech.com",
-                DateOfBirth = new DateTime(1979, 11, 11),
-                Gender = Gender.Female,
-                DepartmentId = 1,
-                PhotoPath = "images/mary.png"
-            };
-
-            Employee e4 = new Employee
-            {
-                EmployeeId = 3,
-                FirstName = "Sara",
-                LastName = "Longway",
-                Email = "sara@pragimtech.com",
-                DateOfBirth = new DateTime(1982, 9, 23),
-                Gender = Gender.Female,
-                DepartmentId = 3,
-                PhotoPath = "images/sara.png"
-            };
-
-            Employees = new List<Employee> { e1, e2, e3, e4 };
+            Employees = (await employeeService.GetEmployees()).ToList();
         }
     }
 }
