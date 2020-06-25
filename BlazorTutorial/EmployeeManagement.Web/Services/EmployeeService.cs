66728af4e453fd507c32e502da49286f11dfb97b
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-25 02:30:47 +0200

    Implemented GetEmployees method in EmployeeService. Also added httpClient field and set its value through DI. In the startup class added httpClient to services with the EmployeeService as well and also defined the client's BaseAddress.

---
+++ BlazorTutorial/EmployeeManagement.Web/Services/EmployeeService.cs
@@ -0,0 +1,24 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly HttpClient httpClient;
+
+        public EmployeeService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return await httpClient.GetJsonAsync<Employee[]>("api/employees");
+        }
+    }
+}
--- BlazorTutorial/EmployeeManagement.Web/Startup.cs
+++ BlazorTutorial/EmployeeManagement.Web/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -25,6 +26,10 @@ namespace EmployeeManagement.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient<IEmployeeService, EmployeeService>(client =>
+            {
+                client.BaseAddress = new Uri("https://localhost:44391/");
+            });
             services.AddRazorPages();
             services.AddServerSideBlazor();
         }
