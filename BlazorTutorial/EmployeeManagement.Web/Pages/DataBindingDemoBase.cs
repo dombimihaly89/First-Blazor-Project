2d223e5fdfc48af839e470b26f8ae005cf67e252
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-26 18:41:21 +0200

    Made a simple DataBindingDemo project

---
+++ BlazorTutorial/EmployeeManagement.Web/Pages/DataBindingDemoBase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Pages
+{
+    public class DataBindingDemoBase : ComponentBase
+    {
+        protected string Name { get; set; } = "Tom";
+        protected string Gender { get; set; } = "Male";
+        protected string Colour { get; set; } = "background-color:red";
+
+        //protected void Name_Change(ChangeEventArgs args)
+        //{
+        //    Name = args.Value.ToString();
+        //}
+    }
+}
