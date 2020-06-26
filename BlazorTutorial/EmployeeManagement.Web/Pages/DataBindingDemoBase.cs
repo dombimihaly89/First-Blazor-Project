9cdf7f0c5a806bddbb0ed07d7e1ee44f53564edc
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-26 18:56:42 +0200

    Added a small example to DataBindingDemo

--- BlazorTutorial/EmployeeManagement.Web/Pages/DataBindingDemoBase.cs
+++ BlazorTutorial/EmployeeManagement.Web/Pages/DataBindingDemoBase.cs
@@ -11,6 +11,7 @@ namespace EmployeeManagement.Web.Pages
         protected string Name { get; set; } = "Tom";
         protected string Gender { get; set; } = "Male";
         protected string Colour { get; set; } = "background-color:red";
+        protected string Description { get; set; }
 
         //protected void Name_Change(ChangeEventArgs args)
         //{
