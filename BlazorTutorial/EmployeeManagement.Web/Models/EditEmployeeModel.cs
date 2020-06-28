e0493c02d2423863ba92a8389866d22b854babc3
Dombi Mihály <dombimihaly89@gmail.com>
2020-06-28 18:42:45 +0200

    Created EditEmployee model in EmployeeManagement.Web which is almost the same as Employee model but it has a ConfirmEmail property. Implemented Email confirmation to EditEmployee page using the CompareProperty attribute on ConfirmEmail property. Also addad a submit button on the form which uses an OnValidSubmit property.

---
+++ BlazorTutorial/EmployeeManagement.Web/Models/EditEmployeeModel.cs
@@ -0,0 +1,30 @@
+﻿using EmployeeManagement.Models;
+using EmployeeManagement.Models.CustomValidators;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Web.Models
+{
+    public class EditEmployeeModel
+    {
+        public int EmployeeId { get; set; }
+        [Required(ErrorMessage = "Firstname must be provided")]
+        [MinLength(2, ErrorMessage = "The name must be at least two characters")]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [EmailAddress]
+        [EmailDomainValidator("com", "The email address has to end with \"com\"")]
+        public string Email { get; set; }
+        [CompareProperty("Email", ErrorMessage = "E-mail fields are not matching")]
+        public string ConfirmEmail { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+        public int DepartmentId { get; set; }
+        public string PhotoPath { get; set; }
+        public Department Department { get; set; }
+    }
+}
--- BlazorTutorial/EmployeeManagement.Web/Pages/EditEmployeeBase.cs
+++ BlazorTutorial/EmployeeManagement.Web/Pages/EditEmployeeBase.cs
@@ -1,4 +1,5 @@
 ﻿using EmployeeManagement.Models;
+using EmployeeManagement.Web.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
 using System;
@@ -19,12 +20,28 @@ namespace EmployeeManagement.Web.Pages
         public string EmployeeId { get; set; }
 
         public Employee Employee { get; set; } = new Employee();
+        public EditEmployeeModel EditEmployeeModel { get; set; } = new EditEmployeeModel();
         public List<Department> Departments { get; set; } = new List<Department>();
 
         protected async override Task OnInitializedAsync()
         {
             Employee = await EmployeeService.GetEmployee(int.Parse(EmployeeId));
             Departments = (await DepartmentService.GetDepartments()).ToList();
+            EditEmployeeModel.EmployeeId = Employee.EmployeeId;
+            EditEmployeeModel.FirstName = Employee.FirstName;
+            EditEmployeeModel.LastName = Employee.LastName;
+            EditEmployeeModel.Email = Employee.Email;
+            EditEmployeeModel.ConfirmEmail = Employee.Email;
+            EditEmployeeModel.DateOfBirth = Employee.DateOfBirth;
+            EditEmployeeModel.Gender = Employee.Gender;
+            EditEmployeeModel.PhotoPath = Employee.PhotoPath;
+            EditEmployeeModel.DepartmentId = Employee.DepartmentId;
+            EditEmployeeModel.Department = Employee.Department;
+        }
+
+        protected void HandleValidSubmit()
+        {
+
         }
     }
 }
